21d477d9d69f3960cecd836d94e42f8dfd2b8ef7
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-05-11 13:15:14 -0300

    Exercise solution

--- src/main/java/KindergartenGarden.java
+++ src/main/java/KindergartenGarden.java
@@ -1,13 +1,37 @@
+import java.util.ArrayList;
 import java.util.List;
 
 class KindergartenGarden {
+    List<Plant> firstRow;
+    List<Plant> secondRow;
+
+    enum Children {
+        Alice, Bob, Charlie, David,
+        Eve, Fred, Ginny, Harriet,
+        Ileana, Joseph, Kincaid, Larry
+    }
 
     KindergartenGarden(String garden) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        firstRow = new ArrayList<>();
+        secondRow = new ArrayList<>();
+
+        var splitGarden = garden.split("\n");
+        splitGarden[0].chars().forEach(n ->
+                this.firstRow.add(Plant.getPlant((char) n))
+        );
+        splitGarden[1].chars().forEach(n ->
+                this.secondRow.add(Plant.getPlant((char) n))
+        );
     }
 
     List<Plant> getPlantsOfStudent(String student) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int cup = Children.valueOf(student).ordinal() * 2;
+        List<Plant> plants = new ArrayList<>();
+        plants.add(firstRow.get(cup));
+        plants.add(firstRow.get(cup + 1));
+        plants.add(secondRow.get(cup));
+        plants.add(secondRow.get(cup + 1));
+        return plants;
     }
 
 }
